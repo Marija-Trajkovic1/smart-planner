5d9e2be0aadba90a62ea6b0c7532a2ba76f972b1
Marija-Trajkovic1 <trajkoviccmarija@gmail.com>
2026-08-11 23:56:49 +0200

    Dodata forma za registraciju novog korisnika

--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatLabel, MatFormField, MatHint } from '@angular/material/form-field';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatCardModule, MatLabel, MatFormField, MatHint, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  constructor(public router:Router) {}
+}
