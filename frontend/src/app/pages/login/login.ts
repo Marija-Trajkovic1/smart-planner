f889eb2e9baa68f19290fb2a2c027354301412fd
Marija-Trajkovic1 <trajkoviccmarija@gmail.com>
2026-08-13 22:11:06 +0200

    Dodata login forma na frontendu

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardHeader, MatCardContent, MatCardTitle, MatCardModule } from "@angular/material/card";
+import { MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatCardModule, 
+    MatCardHeader, 
+    MatCardContent,
+    MatLabel,
+    MatFormField,
+    MatHint,
+    ReactiveFormsModule,
+    MatCardTitle,
+    MatInputModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  private formBuilder = inject(FormBuilder);
+  constructor(){}
+
+  loginForm: FormGroup = this.formBuilder.group({
+    email: ['', 
+      [Validators.required, 
+        Validators.email]
+      ],
+    password: ['' , 
+      [Validators.required, 
+        Validators.minLength(8), 
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).+$/)]
+      ],
+  })
+
+  onLogin():void{}
+}
